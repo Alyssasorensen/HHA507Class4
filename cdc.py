e36b5000f8b1f69574b1e35151e3a076a6699869
Alyssasorensen <-m>
2023-09-20 23:05:11 +0000

    added a cdc.py file and added information

---
+++ cdc.py
@@ -0,0 +1,58 @@
+# Import necessary libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Fetch the data
+url = "https://raw.githubusercontent.com/Alyssasorensen/HHA507Class4/main/datasets/cdc_places_ny.csv"
+df = pd.read_csv(url)
+df.columns
+
+# Filter for 'BINGE' as measureid and 'Age-adjusted prevalence' as data_value_type
+df = df[(df['MeasureId'] == 'BINGE') & (df['Data_Value_Type'] == 'Age-adjusted prevalence')]
+df
+
+# Group by 'LocationName' and get the average (or sum) 'Data_Value'
+grouped = df.groupby('LocationName').Data_Value.mean().sort_values(ascending=False)
+grouped
+
+# Plotting
+plt.figure(figsize=(10, 7))
+grouped.plot(kind='bar', color='lightcoral')
+plt.ylabel('Average Data Value (Age-adjusted prevalence) - Percent')
+plt.xlabel('Location (County)')
+plt.title('Binge Drinking Age-adjusted Prevalence by County in NY')
+plt.xticks(rotation=90)
+plt.tight_layout()
+plt.savefig("binge_drinking_per_location.png")  # Saving the plot as an image
+plt.show()
+
+import ipywidgets as widgets
+
+# Compute the average data value across all counties
+avg_data_value = df['Data_Value'].mean()
+
+# Sort the counties in ascending order for the dropdown list
+sorted_counties = sorted(df['LocationName'].unique())
+
+# Interactive selection of county for visualization using ipywidgets
+@widgets.interact(County=sorted_counties)
+def plot_data(County):
+    county_value = df[df['LocationName'] == County]['Data_Value'].values[0]
+
+    # Labels for bars
+    labels = [County, 'Average across all counties']
+
+    # Data values for bars
+    values = [county_value, avg_data_value]
+
+    plt.figure(figsize=(8, 6))
+
+    # Plot the bars
+    colors = ['lightcoral', 'lightblue']
+    plt.bar(labels, values, color=colors)
+
+    plt.ylabel('Data Value (Age-adjusted prevalence) - Percent')
+    plt.title(f'Binge Drinking Age-adjusted Prevalence in {County} vs Average across all counties')
+
+    plt.tight_layout()
+    plt.show()
